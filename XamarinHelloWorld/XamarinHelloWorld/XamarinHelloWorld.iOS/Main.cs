464f817f421184f263b56c88458cf1ed1daa2964
Deekshit Addepalli <deaddepa@microsoft.com>
2020-10-27 16:27:33 -0500

    First Commit with Solution and project

---
+++ XamarinHelloWorld/XamarinHelloWorld/XamarinHelloWorld.iOS/Main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+
+namespace XamarinHelloWorld.iOS
+{
+    public class Application
+    {
+        // This is the main entry point of the application.
+        // This is a  comment by deaddepa to track my first commit into the DXamarin repo on github
+        static void Main(string[] args)
+        {
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, "AppDelegate");
+        }
+    }
+}
